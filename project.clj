c3527b7c67935c51a16437467a6f46e2cd5ba701
Richard Newman <rnewman@twinql.com>
2010-11-19 10:24:19 -0800

    Bump Clojure dependency to released 1.2.0. Version = 1.1.0.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.twinql.clojure/clj-conneg "1.0.0"
+(defproject com.twinql.clojure/clj-conneg "1.1.0"
  :description "Clojure content negotiation library."
- :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-                [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]])
+ :dependencies [[org.clojure/clojure "1.2.0"]
+                [org.clojure/clojure-contrib "1.2.0"]])
