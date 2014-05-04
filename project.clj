ece6b9367376d246fcffa0611b8dfd6d5a891797
Richard Newman <rnewman@twinql.com>
2014-05-04 11:09:48 -0700

    Add URL and license clause.

--- project.clj
+++ project.clj
@@ -1,3 +1,7 @@
 (defproject com.twinql.clojure/clj-conneg "1.2.0-SNAPSHOT"
  :description "Clojure content negotiation library."
+ :url "https://github.com/rnewman/clj-conneg"
+ :license {:name "BSD 3-clause license"
+           :url "https://github.com/rnewman/clj-conneg/blob/master/LICENSE.txt"
+           :distribution :repo}
  :dependencies [[org.clojure/clojure "1.6.0"]])
